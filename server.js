646d68eff196930a7daaa546b4806faffdcfcf90
Isaiah Hunter <hunter.isaiah96@gmail.com>
2021-07-31 14:08:13 -0400

    added jwt, removed unnecessary variables

--- server.js
+++ server.js
@@ -2,11 +2,8 @@
 const fastify = require('fastify')({ logger: true });
 
 // https://stackoverflow.com/questions/25540711/docker-postgres-pgadmin-local-connection
-
-const path = require('path');
 const fs = require('fs');
 
-const directoryPath = path.join(__dirname, 'models');
 //passsing directoryPath and callback function
 
 fastify.register(
@@ -24,19 +21,19 @@ fastify.register(
             }
         }
     }
-)
+);
 // Register all models
 fs.readdirSync('./models').forEach(file => {
-    fastify.register(require(`./models/${file}`))
+    fastify.register(require(`./models/${file}`));
 });
 // Register all routes
 fs.readdirSync('./routes').forEach(file => {
-    fastify.register(require(`./routes/${file}`))
+    fastify.register(require(`./routes/${file}`));
 });
 
 fastify.register(require('fastify-jwt'), {
     secret: 'supersecret'
-})
+});
 
 const start = async () => {
     try {
